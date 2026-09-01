587e7cd87dbf1cffd7e3cf9f82dfbab1ede04216
Darin <darindivecha@gmail.com>
2026-09-01 18:58:49 +0530

    progress as of 1 sep

---
+++ javascript/chapters/dom/app.js
@@ -0,0 +1,41 @@
+// // DOM - Selecting Elements
+
+// // Select the first element with class "box"
+// let box = document.querySelector(".box");
+
+
+// // console.dir() displays the selected element as a JavaScript object
+// console.dir(box);
+
+
+// // classList - view all classes of an element
+// console.log(box.classList);
+
+
+// // classList.add() - adds a new class to the element
+// box.classList.add("yellowBg");
+
+
+// // classList.remove() - removes a class
+// // box.classList.remove("yellowBg");
+
+
+// // classList.contains() - checks whether an element has a class
+// // console.log(box.classList.contains("box"));
+
+
+// // classList.toggle() - adds the class if it isn't there,
+// // and removes it if it is already there
+// // box.classList.toggle("yellowBg");
+
+// // Selecting an element
+// let box = document.querySelector(".box");
+
+// // Inspecting the element
+// console.dir(box);
+
+// // Viewing its classes
+// console.log(box.classList);
+
+// // Adding a class
+// box.classList.add("yellowBg");
